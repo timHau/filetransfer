adad664dd0fc7b690fcb9ce387a1acfd7537ba1f
timHau <tim.hau@hotmail.de>
2022-07-18 14:04:09 +0200

    Send mail on multi message

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+rm -rf ./tmp
+rm -rf ./assets/*.*
 sed -i '' 's/http:\/\/localhost\:8080/https:\/\/filetransfer\.safespace\.dev/g' .env
 rsync -ayp ./ tau@116.203.84.88:~/filetransfer
 sed -i '' 's/https:\/\/filetransfer\.safespace\.dev/http:\/\/localhost\:8080/g' .env
